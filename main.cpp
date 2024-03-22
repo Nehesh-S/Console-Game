6421c099f56a1eb3f1209659e98a58923ef6ad9a
Nehesh Sawarkar <nehesh07@gmail.com>
2024-03-22 20:28:56 +0530

    init

--- main.cpp
+++ main.cpp
@@ -1,31 +1,15 @@
 #include <iostream>
 #include <SDL2/SDL.h>
 
-const int WIDTH = 800, HEIGHT = 600;
 int main(int argc, char *argv[])
 {
-    SDL_Init(SDL_INIT_EVERYTHING);
-    SDL_Window *window = SDL_CreateWindow("Hello World", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_ALLOW_HIGHDPI);
-
-    if(NULL == window)
+    if(SDL_Init(SDL_INIT_VIDEO) < 0)
     {
-        std::cout << "Error: " << SDL_GetError() << std::endl;
-        return 1;
+        std::cout << "SDL could not be initialized: " << SDL_GetError();
     }
-
-    SDL_Event windowEvent;
-
-    while(true)
+    else
     {
-        if(SDL_PollEvent(&windowEvent))
-        {
-            if(SDL_QUIT == windowEvent.type)
-                break;
-        }
+        std::cout << "SDL video system is ready to go\n";
     }
-
-    SDL_DestroyWindow(window);
-    SDL_Quit();
-
-    return EXIT_SUCCESS;
+    return 0;
 }
