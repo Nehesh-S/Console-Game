5064f7105ce56596f6b56d8950fe84d0cb5d7318
Nehesh Sawarkar <nehesh07@gmail.com>
2024-03-29 13:53:33 +0530

    Betterments

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,7 +1,11 @@
 #include <game.h>
 
 int main(int argc, char* argv[]) {
-    Game game;
+    
+    int _screenWidth = 1024;
+    int _screenHeight = 600;
+    
+    Game game("Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, _screenWidth, _screenHeight, SDL_WINDOW_SHOWN);
     game.run();
 
     return 0;
